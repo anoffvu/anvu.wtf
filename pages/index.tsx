c2ce0b2e5023529d55a0ebd2e2df6c8235e2c747
anoffvu <anoffvu@gmail.com>
2020-09-22 14:45:29 -0500

    home redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 const Index = () => {
-  return <div />;
-};
+  return <div />
+}
 
-Index.getInitialProps = async function(context) {
-  const { res } = context;
+Index.getInitialProps = async function (context) {
+  const { res } = context
   res.writeHead(301, {
-    Location: 'https://www.notion.so/anoffvu/Stupid-Thoughts-ac78f545fd63475e81becbbada28b654'
+    Location: 'an-vu.com'
   })
   res.end()
   return {}
 }
 
-export default Index;
+export default Index
